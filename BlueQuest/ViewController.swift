48c2f560a18cb5686b79cca608ad0d17c0613edf
Caio Oliveira <ccaio.oliveira83@gmail.com>
2026-07-26 22:47:50 -0300

    first commit

---
+++ BlueQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlueQuest
+//
+//  Created by Caio Lucas Oliveira Vieira on 26/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BlueQuestTests/BlueQuestTests.swift
@@ -0,0 +1,19 @@
+//
+//  BlueQuestTests.swift
+//  BlueQuestTests
+//
+//  Created by Caio Lucas Oliveira Vieira on 26/07/26.
+//
+
+import Testing
+@testable import BlueQuest
+
+struct BlueQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

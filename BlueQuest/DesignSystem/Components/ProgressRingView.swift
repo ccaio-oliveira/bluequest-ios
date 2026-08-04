2bb17b626ade7541fdeb7558c87e83b02d4293a5
Caio Oliveira <ccaio.oliveira83@gmail.com>
2026-08-04 11:18:10 -0300

    feat: add home header

---
+++ BlueQuest/DesignSystem/Components/ProgressRingView.swift
@@ -0,0 +1,8 @@
+//
+//  ProgressRingView.swift
+//  BlueQuest
+//
+//  Created by Caio Lucas Oliveira Vieira on 04/08/26.
+//
+
+import Foundation
---
+++ BlueQuest/Features/Home/HomeHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeHeaderView.swift
+//  BlueQuest
+//
+//  Created by Caio Lucas Oliveira Vieira on 04/08/26.
+//
+
+import Foundation
